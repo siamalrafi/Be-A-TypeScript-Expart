ecd4f7b880d8f346455cc0b592aa2028223d710a
siamalrafi <rahathasandipto@gmail.com>
2023-07-22 19:31:14 +0600

    new

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello world!");
+abstract class Vehicle {
+   abstract start(): void;
+   abstract stop(): void;
+
+   move() {
+      console.log("Vehicle is moving.");
+   }
+}
+
+class Car extends Vehicle {
+   start() {
+      console.log("Car started.");
+      
+   }
+
+   stop() {
+      console.log("Car stopped.");
+   }
+}
+
+const car = new Car();
+car.start(); // Output: Car started.
+car.move(); // Output: Vehicle is moving.
+car.stop(); // Output: Car stopped.
